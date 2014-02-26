8168dfe95a8e22ca19f416d0bf0f556dbfe98977
Brian Winterling <bwinterling@yahoo.com>
2014-02-26 11:04:01 -0700

    refactor to singular request - IN PROGRESS

--- lib/usgesus/request.rb
+++ lib/usgesus/request.rb
@@ -1,9 +1,52 @@
 module Usgesus
   class Request
 
+    def self.measurements_by(input, date_range = nil)
+      response = make_api_request(input, date_range)
+      return response if response[:status] == "Invalid Request"
+      time_series = response["value"]["timeSeries"]
+
+      streams = Hash.new
+
+      time_series.each do |ts|
+        site_id = ts["sourceInfo"]["siteCode"].first["value"]
+        values =  ts["values"].first["value"]
+        unit = ts["variable"]["unit"]["unitAbbreviation"]
+        measurements = []
+
+        unless streams.key?(site_id)
+          streams[site_id] = {}
+          streams[site_id][:sitename] = ts["sourceInfo"]["siteName"]
+          streams[site_id][:geolocation] =  ts["sourceInfo"]["geoLocation"]["geogLocation"]
+          streams[site_id][:measurements] = measurements
+        end
+
+        values.each do |value|
+          streams[site_id][:measurements] << {
+            "dateTime" => value["dateTime"],
+            "unit" => unit,
+            "value" => value["value"]
+          }
+        end
+
+      end
+
+
+      streams.map do |gauge_id, data|
+        Usgesus::Gauge.new(
+          gauge_id:  gauge_id,
+          site_name: data[:sitename],
+          geo_location:  data[:geolocation],
+          measurements: data[:measurements]
+        )
+      end
+
+    end
+
     def self.by_gauge(id, date_range = nil)
-      response = site(id, "sites", date_range)
-      time_series = JSON.parse(response.body)["value"]["timeSeries"]
+      response = make_api_request(id, date_range)
+      return response if response[:status] == "Invalid Request"
+      time_series = response["value"]["timeSeries"]
 
       gauge = Usgesus::Gauge.new
       measurements = []
@@ -12,9 +55,15 @@ module Usgesus
         gauge.gauge_id = ts["sourceInfo"]["siteCode"].first["value"]
         gauge.site_name =  ts["sourceInfo"]["siteName"]
         gauge.geo_location = ts["sourceInfo"]["geoLocation"]["geogLocation"]
+        unit = ts["variable"]["unit"]["unitAbbreviation"]
         values = ts["values"].first["value"]
-
-        save_measurements(values, measurements)
+        values.each do |value|
+          measurements << {
+            "dateTime" => value["dateTime"],
+            "unit" => unit,
+            "value" => value["value"]
+          }
+        end
       end
 
       gauge.measurements = measurements
@@ -23,23 +72,25 @@ module Usgesus
 
 
     def self.by_state(state_abbr, date_range=nil)
-      response = site(state_abbr, "stateCD", date_range)
-      time_series = JSON.parse(response.body)["value"]["timeSeries"]
+      response = make_api_request(state_abbr, date_range)
+      time_series = response["value"]["timeSeries"]
+
       streams = Hash.new
 
       time_series.each do |ts|
         site_id = ts["sourceInfo"]["siteCode"].first["value"]
         values =  ts["values"].first["value"]
+        unit = ts["variable"]["unit"]["unitAbbreviation"]
         measurements = []
 
-        unless streams.key?(site_id) 
+        unless streams.key?(site_id)
           streams[site_id] = {}
           streams[site_id][:sitename] = ts["sourceInfo"]["siteName"]
           streams[site_id][:geolocation] =  ts["sourceInfo"]["geoLocation"]["geogLocation"]
           streams[site_id][:measurements] = measurements
         end
 
-        save_measurements(values, measurements)
+        save_measurements(values, unit, measurements)
       end
       save_state_gauges(streams)
     end
@@ -57,29 +108,30 @@ module Usgesus
       end
     end
 
-    def self.site(input, type, date_range=nil)
-      if type == "sites" && date_range
-        Faraday.get(
-          "http://waterservices.usgs.gov/nwis/dv/?format=json&#{type}=#{input}&startDT=#{date_range.first.to_s}&endDT=#{date_range.last.to_s}"
-        )
-      elsif type == "stateCD" && date_range
-        Faraday.get(
-          "http://waterservices.usgs.gov/nwis/dv/?format=json&#{type}=#{input}&startDT=#{date_range.first.to_s}&endDT=#{date_range.last.to_s}"
-        )
-      elsif type == 'sites'
-        Faraday.get(
-          "http://waterservices.usgs.gov/nwis/dv/?format=json&#{type}=#{input}"
-        )
-      elsif type == 'stateCD'
-        Faraday.get("http://waterservices.usgs.gov/nwis/dv/?format=json&#{type}=#{input}")
+    def self.make_api_request(input, date_range=nil)
+      base_url = "http://waterservices.usgs.gov/nwis/dv/?format=json"
+      # testing if State Code or Gauge Id was passed as input
+      if input.length == 2 && date_range
+        request_params = "&stateCD=#{input}&startDT=#{date_range.first.to_s}&endDT=#{date_range.last.to_s}"
+      elsif input.length == 2
+        request_params = "&stateCD=#{input}"
+      elsif input.length == 8 && date_range
+        request_params = "&sites=#{input}&startDT=#{date_range.first.to_s}&endDT=#{date_range.last.to_s}"
+      elsif input.length == 8
+        request_params = "&sites=#{input}"
+      else
+        return {:status => "Invalid Request"}
       end
+
+      response = Faraday.get(base_url + request_params)
+      JSON.parse(response.body)
     end
 
-    def self.save_measurements(values, measurements)
+    def self.save_measurements(values, unit, measurements)
       values.each do |value|
         measurements << {
           "dateTime" => value["dateTime"],
-          "unit" => value["unit"],
+          "unit" => unit,
           "value" => value["value"]
         }
       end
--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -5,39 +5,60 @@ describe Usgesus::Request do
   it "should return recent streamflow by gauge id" do
     gauge_id = "09057500"
     site_name = "BLUE RIVER BELOW GREEN MOUNTAIN RESERVOIR, CO"
-    expect(Usgesus::Request.by_gauge(gauge_id).site_name).to eq site_name
+    response = Usgesus::Request.measurements_by(gauge_id)
+    puts "GaugeOnlyNew: #{response}"
+
+    test_response = Usgesus::Request.by_gauge(gauge_id)
+    puts "GaugeOnlyOld: #{test_response.inspect}"
+
+    expect(response.first.site_name).to eq site_name
   end
 
   it "should return streamflow by gauge id for a specific date" do
     gauge_id = "09057500"
     site_name = "BLUE RIVER BELOW GREEN MOUNTAIN RESERVOIR, CO"
     date_range = ((Date.today - 2)..Date.today)
+    response = Usgesus::Request.measurements_by(gauge_id, date_range).first
+    puts "GaugeDateNew: #{response.inspect}"
 
-    response = Usgesus::Request.by_gauge(gauge_id, date_range)
+    test_response = Usgesus::Request.by_gauge(gauge_id, date_range)
+    puts "GaugeDateOld: #{test_response.inspect}"
+
     measurements = response.measurements
     in_date_range = measurements.all? { |msmt| date_range.include?(Date.parse(msmt["dateTime"])) }
-
     expect(response.site_name).to eq site_name
     expect(in_date_range).to be_true
   end
 
-  it "should return recent streamflow by state" do
+  xit "should return recent streamflow by state" do
     state = "AK"
-    streams = Usgesus::Request.by_state(state)
-    number_of_streams = streams.count
+    response = Usgesus::Request.measurements_by(state)
+    number_of_streams = response.count
     expect(number_of_streams).to eq 534
-    expect(streams.first.gauge_id).to eq "15008000"
+    expect(response.first.gauge_id).to eq "15008000"
   end
 
-  it "should return streamflow by state for a specific date" do
+  xit "should return streamflow by state for a specific date" do
     state = "AK"
     date_range = ((Date.today - 2)..Date.today)
-    streams = Usgesus::Request.by_state(state, date_range)
+    response = Usgesus::Request.measurements_by(state, date_range)
     #site name ends with state code
-    expect(streams.first.site_name[-3..-1]).to include(state)
+    expect(response.first.site_name[-3..-1]).to include(state)
     #measurements date is within our date range
-   streams.first.measurements.each do |m|
-     expect(date_range).to include(Date.parse(m["dateTime"]))
-   end
+    response.first.measurements.each do |m|
+      expect(date_range).to include(Date.parse(m["dateTime"]))
+    end
   end
+
+  xit "should return Invalid Request if statecode or gauge_id are incorrect" do
+    response = Usgesus::Request.measurements_by("BOOM")
+    expect(response[:status]).to eq "Invalid Request"
+  end
+
+  xit "should make request for either state or gauge_id" do
+    gauge_id = "09057500"
+    site_name = "BLUE RIVER BELOW GREEN MOUNTAIN RESERVOIR, CO"
+    expect(Usgesus::Request.measurements_by(gauge_id).first.site_name).to eq site_name
+  end
+
 end
