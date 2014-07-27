429789931c55fef5f67f13ac1ae9b61fa987ec43
Adam89 <adam.dev89@gmail.com>
2014-07-27 23:54:59 +0100

    Refactor `.measurements_by`
    Start breaking up this huge method

--- lib/usgs/request.rb
+++ lib/usgs/request.rb
@@ -1,6 +1,5 @@
 module Usgs
   class Request
-
     STATE_CODES = {
       "01"=>"AL","02"=>"AK","04"=>"AZ","05"=>"AR","06"=>"CA","08"=>"CO",
       "09"=>"CT","10"=>"DE","11"=>"DC","12"=>"FL","13"=>"GA","15"=>"HI",
@@ -17,6 +16,7 @@ module Usgs
       response = make_api_request(input, date_range)
       return response if response[:status] == "Invalid Request"
       time_series = response["value"]["timeSeries"]
+
       # create a hash using gauge_id as the key
       # this accounts for the USGS api response where multiple timeSeries
       # objects exist for the same gauge_id
@@ -24,17 +24,15 @@ module Usgs
       time_series.each do |ts|
         site_id = ts["sourceInfo"]["siteCode"].first["value"]
         state_property = ts["sourceInfo"]["siteProperty"].find { |p| p["name"] == "stateCd" }
-        state = STATE_CODES[state_property["value"]]
         values =  ts["values"].first["value"]
         unit = ts["variable"]["unit"]["unitAbbreviation"]
-        measurements = []
 
         unless streams.key?(site_id)
           streams[site_id] = {}
           streams[site_id][:sitename] = ts["sourceInfo"]["siteName"]
-          streams[site_id][:state] = state
+          streams[site_id][:state] = STATE_CODES[state_property["value"]]
           streams[site_id][:geolocation] =  ts["sourceInfo"]["geoLocation"]["geogLocation"]
-          streams[site_id][:measurements] = measurements
+          streams[site_id][:measurements] = []
         end
 
         values.each do |value|
@@ -46,24 +44,21 @@ module Usgs
         end
       end
 
-      # convert the hash to individual gauge objects
-      streams.map do |gauge_id, data|
-        Usgs::Gauge.new(
-          gauge_id:  gauge_id,
-          site_name: data[:sitename],
-          state: data[:state],
-          geo_location:  data[:geolocation],
-          measurements: data[:measurements]
-        )
-      end
-
+      create_gauges(streams)
     end
 
     private
 
     def self.make_api_request(input, date_range=nil)
+      params = generate_url(input, date_range)
+
+      response = Faraday.get(params)
+      JSON.parse(response.body)
+    end
+
+    def self.generate_url(input, date_range=nil)
       base_url = "http://waterservices.usgs.gov/nwis/dv/?format=json"
-      # testing if State Code or Gauge Id was passed as input
+
       if input.length == 2 && date_range
         request_params = "&stateCd=#{input}&startDT=#{date_range.first.to_s}&endDT=#{date_range.last.to_s}"
       elsif input.length == 2
@@ -76,8 +71,19 @@ module Usgs
         return {:status => "Invalid Request"}
       end
 
-      response = Faraday.get(base_url + request_params)
-      JSON.parse(response.body)
+      base_url + request_params
+    end
+
+    def self.create_gauges(streams)
+      streams.map do |gauge_id, data|
+        Usgs::Gauge.new(
+          gauge_id:  gauge_id,
+          site_name: data[:sitename],
+          state: data[:state],
+          geo_location:  data[:geolocation],
+          measurements: data[:measurements]
+        )
+      end
     end
   end
 end
