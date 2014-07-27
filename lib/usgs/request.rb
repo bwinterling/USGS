573c79e7ea35c7e53fac216109613b9667a9e4db
Adam89 <adam.dev89@gmail.com>
2014-07-27 01:22:47 +0100

    Move States into constant
    Since a States code number and name does not change it
    would be best represented by a constant.

--- lib/usgs/request.rb
+++ lib/usgs/request.rb
@@ -1,6 +1,18 @@
 module Usgs
   class Request
 
+    STATE_CODES = {
+      "01"=>"AL","02"=>"AK","04"=>"AZ","05"=>"AR","06"=>"CA","08"=>"CO",
+      "09"=>"CT","10"=>"DE","11"=>"DC","12"=>"FL","13"=>"GA","15"=>"HI",
+      "16"=>"ID","17"=>"IL","18"=>"IN","19"=>"IA","20"=>"KS","21"=>"KY",
+      "22"=>"LA","23"=>"ME","24"=>"MD","25"=>"MA","26"=>"MI","27"=>"MN",
+      "28"=>"MS","29"=>"MO","30"=>"MT","31"=>"NE","32"=>"NV","33"=>"NH",
+      "34"=>"NJ","35"=>"NM","36"=>"NY","37"=>"NC","38"=>"ND","39"=>"OH",
+      "40"=>"OK","41"=>"OR","42"=>"PA","44"=>"RI","45"=>"SC","46"=>"SD",
+      "47"=>"TN","48"=>"TX","49"=>"UT","50"=>"VT","51"=>"VA","53"=>"WA",
+      "54"=>"WV","55"=>"WI","56"=>"WY"
+    }
+
     def self.measurements_by(input, date_range = nil)
       response = make_api_request(input, date_range)
       return response if response[:status] == "Invalid Request"
@@ -12,7 +24,7 @@ module Usgs
       time_series.each do |ts|
         site_id = ts["sourceInfo"]["siteCode"].first["value"]
         state_property = ts["sourceInfo"]["siteProperty"].find { |p| p["name"] == "stateCd" }
-        state = state_lookup[state_property["value"]]
+        state = STATE_CODES[state_property["value"]]
         values =  ts["values"].first["value"]
         unit = ts["variable"]["unit"]["unitAbbreviation"]
         measurements = []
@@ -47,7 +59,7 @@ module Usgs
 
     end
 
-  private
+    private
 
     def self.make_api_request(input, date_range=nil)
       base_url = "http://waterservices.usgs.gov/nwis/dv/?format=json"
@@ -67,20 +79,5 @@ module Usgs
       response = Faraday.get(base_url + request_params)
       JSON.parse(response.body)
     end
-
-    def self.state_lookup
-      {
-        "01"=>"AL","02"=>"AK","04"=>"AZ","05"=>"AR","06"=>"CA","08"=>"CO",
-        "09"=>"CT","10"=>"DE","11"=>"DC","12"=>"FL","13"=>"GA","15"=>"HI",
-        "16"=>"ID","17"=>"IL","18"=>"IN","19"=>"IA","20"=>"KS","21"=>"KY",
-        "22"=>"LA","23"=>"ME","24"=>"MD","25"=>"MA","26"=>"MI","27"=>"MN",
-        "28"=>"MS","29"=>"MO","30"=>"MT","31"=>"NE","32"=>"NV","33"=>"NH",
-        "34"=>"NJ","35"=>"NM","36"=>"NY","37"=>"NC","38"=>"ND","39"=>"OH",
-        "40"=>"OK","41"=>"OR","42"=>"PA","44"=>"RI","45"=>"SC","46"=>"SD",
-        "47"=>"TN","48"=>"TX","49"=>"UT","50"=>"VT","51"=>"VA","53"=>"WA",
-        "54"=>"WV","55"=>"WI","56"=>"WY"
-      }
-    end
-
   end
 end
